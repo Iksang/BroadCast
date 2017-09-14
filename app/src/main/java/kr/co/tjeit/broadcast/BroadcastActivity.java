55c5ece5908de54ad4f52d51d54f53886b2cb3a4
Iksang <thsdlrtkd@gmail.com>
2017-09-14 18:17:51 +0900

    동적 리시버도 구현했다2

--- app/src/main/java/kr/co/tjeit/broadcast/BroadcastActivity.java
+++ app/src/main/java/kr/co/tjeit/broadcast/BroadcastActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class BroadcastActivity extends BaseActivity {
@@ -31,6 +32,20 @@ public class BroadcastActivity extends BaseActivity {
         mBroadcastReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
+
+//                리시버는 10초이내에 동작을 마무리해야함. Timeout
+//                젤리빈 버젼 이후로는 60초로 제한이 늘어남
+//                for문 잘못쓰면 안됨
+
+                Toast.makeText(context, "리시버의 시작", Toast.LENGTH_SHORT).show();
+
+
+                try {
+                    Thread.sleep(12000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
                 String fileName = intent.getStringExtra("fileName");
 
                 Toast.makeText(context, fileName+"이(가) 다운로드 되었습니다.", Toast.LENGTH_SHORT).show();
---
+++ app/src/main/java/kr/co/tjeit/broadcast/ScreenOnReceiver.java
@@ -0,0 +1,16 @@
+package kr.co.tjeit.broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by tjoeun on 2017-09-14.
+ */
+
+public class ScreenOnReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
